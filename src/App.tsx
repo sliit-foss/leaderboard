74f38e1e6ca64a8d5ac91acf9865a61a7f8ba6c4
Shehanka <hcsperera@outlook.com>
2021-01-31 01:36:02 +0530

    Sample Grid Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./components/Common/Footer/Footer";
 import Navbar from "./components/Common/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import { createHashHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import { routes } from "./routes/AppRoutes";
+
+/**
+ * Create history object to pass into Router,
+ * to allow navigating outside of react
+ */
+const history = createHashHistory();
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar/>
+  return (
+    <div>
+      <Navbar />
+      <Router history={history}>
+        <Switch>
+          {routes.map((route: { path: string; component: any }) => (
+            <Route
+              key={route.path}
+              exact
+              path={route.path}
+              component={route.component}
+            />
+          ))}
 
-
-        </div>
-    );
+          <Route key={""} exact path={""} component={HomePage} />
+        </Switch>
+      </Router>
+      <Footer />
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
