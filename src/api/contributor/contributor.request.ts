3e6ad167adca0d43c69e9a7b905c90ba0ae36779
Akalanka <akalankaperera128@gmail.com>
2023-03-19 09:38:20 +0530

    Cleaned dependencies

--- src/api/contributor/contributor.request.ts
+++ src/api/contributor/contributor.request.ts
@@ -2,7 +2,7 @@ import { Contributor } from "../../models/contributor.model";
 import { apiInstance } from "../apiInstance";
 
 export async function getAllContributors() {
-  const PATH = "/leaderboard";
+  const PATH = "/v1/leaderboard";
   try {
     const res = await apiInstance.get(PATH);
     return res.data.data as Contributor[];
