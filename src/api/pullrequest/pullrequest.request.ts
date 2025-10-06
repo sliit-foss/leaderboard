e1ceeddaee37d1c6ac6830060b40d179b1372c8d
moaalk <mohamed.asath@ifs.com>
2025-10-06 11:05:09 +0530

    fix: use GitHub API directly for pending PRs
    - The backend API doesn't have a /pending-prs endpoint
    
    - Fetch pending PRs directly from GitHub REST API
    
    - This eliminates the 404 error when fetching pending PRs

--- src/api/pullrequest/pullrequest.request.ts
+++ src/api/pullrequest/pullrequest.request.ts
@@ -1,6 +1,13 @@
-import { apiInstance } from "../apiInstance";
+import axios from "axios";
+import { PullRequest } from "../../models/pullrequest.model";
 
-export const getPendingPRs = async () => {
-  const response = await apiInstance.get("/pending-prs");
+const GITHUB_API_BASE_URL = "https://api.github.com";
+const REPO_OWNER = "sliit-foss";
+const REPO_NAME = "leaderboard";
+
+export const getPendingPRs = async (): Promise<PullRequest[]> => {
+  const response = await axios.get<PullRequest[]>(
+    `${GITHUB_API_BASE_URL}/repos/${REPO_OWNER}/${REPO_NAME}/pulls?state=open`
+  );
   return response.data;
 };
